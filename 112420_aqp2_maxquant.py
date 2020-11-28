c8c8f023704ff1db328b8a94eb3cadebab1a0f76
Kirby L <krbyktl@users.noreply.github.com>
2020-11-28 12:14:33 -0500

    Add files via upload

--- 112420_aqp2_maxquant.py
+++ 112420_aqp2_maxquant.py
@@ -26,6 +26,12 @@ TMT1 = ['C1','C2','C3','C4','V1','V2','V3','V4']
 norm_factor1 = [1.01, 0.91, 0.96, 1.03, 1, 0.95, 1, 1]
 for i in range(len(TMT1)):
     df_TMT1[TMT1[i]] = norm_factor1[i]*df_TMT1[TMT1[i]]
+df_TMT1['T1'] = np.log2(df_TMT1['V1']/df_TMT1['C1'])
+df_TMT1['T2'] = np.log2(df_TMT1['V2']/df_TMT1['C2'])
+df_TMT1['T3'] = np.log2(df_TMT1['V3']/df_TMT1['C3'])
+df_TMT1['T4'] = np.log2(df_TMT1['V4']/df_TMT1['C4'])
+
+
     
 df_TMT2 = df_aqp2[df_aqp2['Replicate'] == 'TMT2']
 df_TMT2 = df_TMT2.rename(columns = {'Reporter intensity corrected 1':'V1',
@@ -40,6 +46,10 @@ TMT2 = ['V1','V2','V3','V4','C1','C2','C3','C4']
 norm_factor2 = [0.96, 1.05, 0.96, 1.13, 1, 1, 0.92, 1.07]
 for i in range(len(TMT2)):
     df_TMT2[TMT2[i]] = norm_factor2[i]*df_TMT2[TMT2[i]]
+df_TMT2['T1'] = np.log2(df_TMT2['V1']/df_TMT2['C1'])
+df_TMT2['T2'] = np.log2(df_TMT2['V2']/df_TMT2['C2'])
+df_TMT2['T3'] = np.log2(df_TMT2['V3']/df_TMT2['C3'])
+df_TMT2['T4'] = np.log2(df_TMT2['V4']/df_TMT2['C4'])
     
 df_TMT3 = df_aqp2[df_aqp2['Replicate'] == 'TMT3']
 df_TMT3 = df_TMT3.rename(columns = {'Reporter intensity corrected 1':'C4',
@@ -54,7 +64,83 @@ TMT3 = ['C4','C3','C2','C1','V4','V3','V2','V1']
 norm_factor3 = [0.99, 1.20, 0.96, 0.95, 1.21, 1.01, 1, 1]
 for i in range(len(TMT3)):
     df_TMT3[TMT3[i]] = norm_factor3[i]*df_TMT3[TMT3[i]]
+df_TMT3['T1'] = np.log2(df_TMT3['V1']/df_TMT3['C1'])
+df_TMT3['T2'] = np.log2(df_TMT3['V2']/df_TMT3['C2'])
+df_TMT3['T3'] = np.log2(df_TMT3['V3']/df_TMT3['C3'])
+df_TMT3['T4'] = np.log2(df_TMT3['V4']/df_TMT3['C4'])
+    
 
-#single phosphosites
+#TMT1 analysis
+df_TMT1_1 = df_TMT1[df_TMT1['Phospho (STY)'] == 1]
+df_TMT1_S256 = pd.concat([df_TMT1_1[df_TMT1_1['Modified sequence'] == '_QS(Phospho (STY))VELHSPQSLPR_'],
+                          df_TMT1_1[df_TMT1_1['Modified sequence'] == '_RQS(Phospho (STY))VELHSPQSLPR_'],
+                          df_TMT1_1[df_TMT1_1['Modified sequence'] == '_RRQS(Phospho (STY))VELHSPQSLPR_']])
+df_TMT1_S261 = pd.concat([df_TMT1_1[df_TMT1_1['Modified sequence'] == '_QSVELHS(Phospho (STY))PQSLPR_'],
+                          df_TMT1_1[df_TMT1_1['Modified sequence'] == '_RQSVELHS(Phospho (STY))PQSLPR_']])
+df_TMT1_S264 = df_TMT1_1[df_TMT1_1['Modified sequence'] == '_QSVELHSPQS(Phospho (STY))LPR_']
+
+df_TMT1_2 = df_TMT1[df_TMT1['Phospho (STY)'] == 2]
+df_TMT1_S256_S261 = pd.concat([df_TMT1_2[df_TMT1_2['Modified sequence'] == '_QS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_'],
+                               df_TMT1_2[df_TMT1_2['Modified sequence'] == '_RQS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_'],
+                               df_TMT1_2[df_TMT1_2['Modified sequence'] == '_RRQS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_']])
+df_TMT1_S261_S264 = df_TMT1_2[df_TMT1_2['Modified sequence'] == '_QSVELHS(Phospho (STY))PQS(Phospho (STY))LPR_']
+df_TMT1_S256_S264 = pd.concat([df_TMT1_2[df_TMT1_2['Modified sequence'] == '_RQS(Phospho (STY))VELHSPQS(Phospho (STY))LPR_'],
+                               df_TMT1_2[df_TMT1_2['Modified sequence'] == '_QS(Phospho (STY))VELHSPQS(Phospho (STY))LPR_']])
+
+df_TMT1_S256_S261_S264 = df_TMT1[df_TMT1['Phospho (STY)'] == 3]
+
+#TMT2 analysis
+df_TMT2_1 = df_TMT2[df_TMT2['Phospho (STY)'] == 1]
+df_TMT2_S256 = pd.concat([df_TMT2_1[df_TMT2_1['Modified sequence'] == '_QS(Phospho (STY))VELHSPQSLPR_'],
+                          df_TMT2_1[df_TMT2_1['Modified sequence'] == '_RQS(Phospho (STY))VELHSPQSLPR_'],
+                          df_TMT2_1[df_TMT2_1['Modified sequence'] == '_RRQS(Phospho (STY))VELHSPQSLPR_']])
+df_TMT2_S261 = pd.concat([df_TMT2_1[df_TMT2_1['Modified sequence'] == '_QSVELHS(Phospho (STY))PQSLPR_'],
+                          df_TMT2_1[df_TMT2_1['Modified sequence'] == '_RQSVELHS(Phospho (STY))PQSLPR_']])
+df_TMT2_S264 = df_TMT2_1[df_TMT2_1['Modified sequence'] == '_QSVELHSPQS(Phospho (STY))LPR_']
+
+df_TMT2_2 = df_TMT2[df_TMT2['Phospho (STY)'] == 2]
+df_TMT2_S256_S261 = pd.concat([df_TMT2_2[df_TMT2_2['Modified sequence'] == '_QS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_'],
+                               df_TMT2_2[df_TMT2_2['Modified sequence'] == '_RQS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_'],
+                               df_TMT2_2[df_TMT2_2['Modified sequence'] == '_RRQS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_']])
+df_TMT2_S261_S264 = df_TMT2_2[df_TMT2_2['Modified sequence'] == '_QSVELHS(Phospho (STY))PQS(Phospho (STY))LPR_']
+df_TMT2_S256_S264 = pd.concat([df_TMT2_2[df_TMT2_2['Modified sequence'] == '_RQS(Phospho (STY))VELHSPQS(Phospho (STY))LPR_'],
+                               df_TMT2_2[df_TMT2_2['Modified sequence'] == '_QS(Phospho (STY))VELHSPQS(Phospho (STY))LPR_'],
+                               df_TMT2_2[df_TMT2_2['Modified sequence'] == '_RRQS(Phospho (STY))VELHSPQS(Phospho (STY))LPR_']])
+
+df_TMT2_S256_S261_S264 = df_TMT2[df_TMT2['Phospho (STY)'] == 3]
+
+#TMT3 analysis
+df_TMT3_1 = df_TMT3[df_TMT3['Phospho (STY)'] == 1]
+df_TMT3_S256 = pd.concat([df_TMT3_1[df_TMT3_1['Modified sequence'] == '_QS(Phospho (STY))VELHSPQSLPR_'],
+                          df_TMT3_1[df_TMT3_1['Modified sequence'] == '_RQS(Phospho (STY))VELHSPQSLPR_'],
+                          df_TMT3_1[df_TMT3_1['Modified sequence'] == '_RRQS(Phospho (STY))VELHSPQSLPR_']])
+df_TMT3_S261 = pd.concat([df_TMT3_1[df_TMT3_1['Modified sequence'] == '_QSVELHS(Phospho (STY))PQSLPR_'],
+                          df_TMT3_1[df_TMT3_1['Modified sequence'] == '_RQSVELHS(Phospho (STY))PQSLPR_']])
+df_TMT3_S264 = df_TMT3_1[df_TMT3_1['Modified sequence'] == '_QSVELHSPQS(Phospho (STY))LPR_']
+#omit GLEPDTDWEER peptides
+
+df_TMT3_2 = df_TMT3[df_TMT3['Phospho (STY)'] == 2]
+df_TMT3_S256_S261 = pd.concat([df_TMT3_2[df_TMT3_2['Modified sequence'] == '_QS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_'],
+                               df_TMT3_2[df_TMT3_2['Modified sequence'] == '_RQS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_'],
+                               df_TMT3_2[df_TMT3_2['Modified sequence'] == '_RRQS(Phospho (STY))VELHS(Phospho (STY))PQSLPR_']])
+df_TMT3_S261_S264 = df_TMT3_2[df_TMT3_2['Modified sequence'] == '_QSVELHS(Phospho (STY))PQS(Phospho (STY))LPR_']
+df_TMT3_S256_S264 = pd.concat([df_TMT3_2[df_TMT3_2['Modified sequence'] == '_RQS(Phospho (STY))VELHSPQS(Phospho (STY))LPR_'],
+                               df_TMT3_2[df_TMT3_2['Modified sequence'] == '_QS(Phospho (STY))VELHSPQS(Phospho (STY))LPR_']])
+
+df_TMT3_S256_S261_S264 = df_TMT3[df_TMT3['Phospho (STY)'] == 3]
+
+#Calculations between the 3 replicates
+S256 = pd.concat([df_TMT1_S256, df_TMT2_S256, df_TMT3_S256])
+S261 = pd.concat([df_TMT1_S261, df_TMT2_S261, df_TMT3_S261])
+S264 = pd.concat([df_TMT1_S264, df_TMT2_S264, df_TMT3_S264])
+S256_S261 = pd.concat([df_TMT1_S256_S261, df_TMT2_S256_S261, df_TMT3_S256_S261])
+S261_S264 = pd.concat([df_TMT1_S261_S264, df_TMT2_S261_S264, df_TMT3_S261_S264])
+S256_S264 = pd.concat([df_TMT1_S256_S264, df_TMT2_S256_S264, df_TMT3_S256_S264])
+S256_S261_S264 = pd.concat([df_TMT1_S256_S261_S264, df_TMT2_S256_S261_S264,
+                           df_TMT3_S256_S261_S264])
+
+#value comparison
+
+
 
 
