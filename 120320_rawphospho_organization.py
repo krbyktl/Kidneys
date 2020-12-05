a719fce417253669071b50f3c72369a155f54a55
Kirby L <krbyktl@users.noreply.github.com>
2020-12-05 12:26:26 -0500

    Add files via upload

--- 120320_rawphospho_organization.py
+++ 120320_rawphospho_organization.py
@@ -6,10 +6,92 @@ Created on Thu Dec  3 10:24:01 2020
 
 more organization of raw MaxQuant data
 """
+#Aqp2 redo for Andromeda
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+fileloc1 = "J:\Depot - dDAVP-time course - Kirby\Analysis\\Aqp2_Msms.xlsx"
+df_aqp2 = pd.read_excel(fileloc1, sheet_name="Aqp2_msms")
+
+df_aqp2 = df_aqp2[~df_aqp2['Raw file'].str.contains('Total')]
+
+df_TMT1 = df_aqp2[df_aqp2['Raw file'].str.contains('TMT1')]
+df_TMT1 = df_TMT1.rename(columns = {'Reporter intensity corrected 1':'C1',
+                         'Reporter intensity corrected 2':'C2',
+                         'Reporter intensity corrected 3':'C3',
+                         'Reporter intensity corrected 4':'C4',
+                         'Reporter intensity corrected 8':'V1',
+                         'Reporter intensity corrected 9':'V2',
+                         'Reporter intensity corrected 10':'V3',
+                         'Reporter intensity corrected 11':'V4'})
+TMT1 = ['C1','C2','C3','C4','V1','V2','V3','V4']
+norm_factor1 = [1.01, 0.91, 0.96, 1.03, 1, 0.95, 1, 1]
+for i in range(len(TMT1)):
+    df_TMT1[TMT1[i]] = norm_factor1[i]*df_TMT1[TMT1[i]]
+
+df_TMT2 = df_aqp2[df_aqp2['Raw file'].str.contains('TMT2')]
+df_TMT2 = df_TMT2.rename(columns = {'Reporter intensity corrected 1':'V1',
+                         'Reporter intensity corrected 2':'V2',
+                         'Reporter intensity corrected 3':'V3',
+                         'Reporter intensity corrected 4':'V4',
+                         'Reporter intensity corrected 8':'C1',
+                         'Reporter intensity corrected 9':'C2',
+                         'Reporter intensity corrected 10':'C3',
+                         'Reporter intensity corrected 11':'C4'})
+TMT2 = ['V1','V2','V3','V4','C1','C2','C3','C4']
+norm_factor2 = [0.96, 1.05, 0.96, 1.13, 1, 1, 0.92, 1.07]
+for i in range(len(TMT2)):
+    df_TMT2[TMT2[i]] = norm_factor2[i]*df_TMT2[TMT2[i]]
+
+df_TMT3 = df_aqp2[df_aqp2['Raw file'].str.contains('TMT3')]
+df_TMT3 = df_TMT3.rename(columns = {'Reporter intensity corrected 1':'C4',
+                         'Reporter intensity corrected 2':'C3',
+                         'Reporter intensity corrected 3':'C2',
+                         'Reporter intensity corrected 4':'C1',
+                         'Reporter intensity corrected 8':'V4',
+                         'Reporter intensity corrected 9':'V3',
+                         'Reporter intensity corrected 10':'V2',
+                         'Reporter intensity corrected 11':'V1'})
+TMT3 = ['C4','C3','C2','C1','V4','V3','V2','V1']
+norm_factor3 = [0.99, 1.20, 0.96, 0.95, 1.21, 1.01, 1, 1]
+for i in range(len(TMT3)):
+    df_TMT3[TMT3[i]] = norm_factor3[i]*df_TMT3[TMT3[i]]
+
+df_aqp2_scale = pd.concat([df_TMT1, df_TMT2, df_TMT3])
+monophos = df_aqp2_scale[df_aqp2_scale['Phospho (STY)'] == 1]
+diphos = df_aqp2_scale[df_aqp2_scale['Phospho (STY)'] == 2]
+triphos = df_aqp2_scale[df_aqp2_scale['Phospho (STY)'] == 3]
+
+mono_uniq_seq = pd.unique(monophos['Modified sequence'])
+di_uniq_seq = pd.unique(diphos['Modified sequence'])
+tri_uniq_seq = pd.unique(triphos['Modified sequence'])
+
+
+from openpyxl import load_workbook
+fileloc1 = "J:\Depot - dDAVP-time course - Kirby\Analysis\\Aqp2_Msms_1.xlsx"
+book = load_workbook(fileloc1)
+writer = pd.ExcelWriter(fileloc1, engine = 'openpyxl')
+writer.book = book
+for i in mono_uniq_seq:
+    monophos[monophos['Modified sequence'] == i].to_excel(writer,
+                                                          sheet_name = str(list(mono_uniq_seq).index(i)) + '_1')
+for j in di_uniq_seq:
+    diphos[diphos['Modified sequence'] == j].to_excel(writer,
+                                                      sheet_name = str(list(di_uniq_seq).index(j)) + '_2')
+for k in tri_uniq_seq:
+    triphos[triphos['Modified sequence'] == k].to_excel(writer,
+                                                      sheet_name = str(list(tri_uniq_seq).index(k)) + '_3')
+
+writer.save()
+writer.close()
+
+
+
+
+
+
+
+
+
+
 
 #Clip1 analysis
 fileloc1 = "J:\Depot - dDAVP-time course - Kirby\Analysis\\Clip1_Msms.xlsx"
