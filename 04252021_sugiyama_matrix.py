0aace65b2433bdfdf6ce2e4940d27786de27d8f1
Kirby L <krbyktl@users.noreply.github.com>
2021-05-02 14:26:06 -0400

    Add files via upload

--- 04252021_sugiyama_matrix.py
+++ 04252021_sugiyama_matrix.py
@@ -101,7 +101,6 @@ IC_array = list(range(len(kinases)))
 for b in IC_array:
     IC_array[b] = freq_array_mod[b]*np.log2(freq_array_mod[b]/STbkgd_array)
 
-kinase_IC_dict = {k:v for k,v in zip(kinases,IC_array)}
 
 #extract clusters
 fileloc2 = "J:\Depot - dDAVP-time course - Kirby\BayesAnalysis\\TC rat IMCD clusters.xlsx"
@@ -233,3 +232,71 @@ writer.book = book
 corr_df.to_excel(writer, sheet_name = 'pearson corr ranking')
 writer.save()
 writer.close()
+
+
+#create a list of random peptides
+import random
+AA_noJ = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
+randpep_list = list(range(12))
+for r in range(len(randpep_list)):
+    rand_AA = list(range(1000))
+    for w in range(len(rand_AA)):
+        base = np.random.choice(AA_noJ).tolist()
+        rand_AA[w] = base
+    randpep_list[r] = rand_AA
+randpep = np.transpose(np.asarray(randpep_list))
+randpep = pd.DataFrame(randpep)
+ns = (randpep.groupby([0]).size()/randpep.groupby([0]).size().sum()).to_dict()
+nf = (randpep.groupby([1]).size()/randpep.groupby([1]).size().sum()).to_dict()
+nfr = (randpep.groupby([2]).size()/randpep.groupby([2]).size().sum()).to_dict()
+nt = (randpep.groupby([3]).size()/randpep.groupby([3]).size().sum()).to_dict()
+ntw = (randpep.groupby([4]).size()/randpep.groupby([4]).size().sum()).to_dict()
+no = (randpep.groupby([5]).size()/randpep.groupby([5]).size().sum()).to_dict()
+o = (randpep.groupby([6]).size()/randpep.groupby([6]).size().sum()).to_dict()
+tw = (randpep.groupby([7]).size()/randpep.groupby([7]).size().sum()).to_dict()
+t = (randpep.groupby([8]).size()/randpep.groupby([8]).size().sum()).to_dict()
+fr = (randpep.groupby([9]).size()/randpep.groupby([9]).size().sum()).to_dict()
+f = (randpep.groupby([10]).size()/randpep.groupby([10]).size().sum()).to_dict()
+s = (randpep.groupby([11]).size()/randpep.groupby([11]).size().sum()).to_dict()
+freqrand = [ns,nf,nfr,nt,ntw,no,o,tw,t,fr,f,s]
+for j in freqrand: 
+    for k in range(len(AA_noJ)):
+        if AA_noJ[k] not in j:
+            j[AA_noJ[k]] = 0  
+M = 20
+#columns
+N = 12
+init = [ [ 0 for i in range(M) ] for j in range(N) ]
+for l in range(M): 
+    for m in range(N):
+        init[m][l] = freqrand[m][AA_noJ[l]]      
+randpep_array =  np.transpose(np.asarray(init))
+IC_randpep = randpep_array*np.log2(randpep_array/IMCDbkgd_array)
+ravel_randpep = np.nan_to_num(np.ravel(IC_randpep))
+corr_randpep = list(range(len(ravel_kinase)))
+for y in range(len(ravel_kinase)):
+    corr_randpep[y] = np.corrcoef(ravel_randpep,ravel_kinase[y])[0][1]
+    
+#sum of information content for positions
+kinase_sums = []
+for v in range(12):
+    pos_sums = 0
+    for b in range(len(IC_array)):
+        pos_sums = pos_sums + sum(abs(np.nan_to_num(np.transpose(IC_array[b])[v])))
+    kinase_sums.append(pos_sums)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+kinase_sums.insert(6,0)
+positions = ['-6','-5','-4','-3','-2','-1','0','+1','+2','+3','+4','+5','+6']
+pos_weight = plt.bar(positions,kinase_sums, color = 'grey')
+pos_weight = sns.set_style('white')
+pos_weight = sns.set_style('ticks')
+pos_weight = sns.despine()
+plt.ylabel('Bits', fontsize = 18)
+plt.xlabel('Position relative to phosphosite', fontsize = 18)
+plt.tight_layout()
+plt.savefig("J:\Depot - dDAVP-time course - Kirby\Figures\IC position weight",
+            dpi=600)
+    
